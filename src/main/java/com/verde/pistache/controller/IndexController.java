8bead175b675940f67b1185ab20f733033f289fd
mq12 <mq12@192.168.1.68>
2018-06-19 17:33:29 -0500

    alberto corona

---
+++ src/main/java/com/verde/pistache/controller/IndexController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.verde.pistache.controller;
+
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author mq12
+ */
+@Controller
+public class IndexController {
+     @RequestMapping(value = {"/"}, method = RequestMethod.GET)
+    String  home(ModelMap model , HttpSession session){
+    
+    return "index";
+    }
+}
